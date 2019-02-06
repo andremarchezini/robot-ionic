7dcf6c163124211088d6d2bff351bf31a991ceac
Unknown <andremarchezini@gmail.com>
2019-02-06 15:24:07 +0800

    added karma coverage

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -1,8 +1,8 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
-
 module.exports = function (config) {
     config.set({
+        files: ['src/**/*.js', 'test/**/*.js'],
         basePath: '',
         frameworks: ['jasmine', '@angular-devkit/build-angular'],
         plugins: [
@@ -10,9 +10,9 @@ module.exports = function (config) {
             require('karma-jasmine-html-reporter'), require('karma-coverage-istanbul-reporter'),
             require('@angular-devkit/build-angular/plugins/karma')
         ],
-        client: {
-            clearContext: false // leave Jasmine Spec Runner output visible in browser
-        },
+        preprocessors: { 'src/**/*.js': ['coverage'] },
+        coverageReporter: { type: 'html', dir: 'coverage/' },
+        client: { clearContext: false },
         coverageIstanbulReporter: {
             dir: require('path').join(__dirname, '../coverage'),
             reports: ['html', 'lcovonly'],
